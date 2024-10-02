6a70b305825a722e169b3a9185d04d2b1b3d6e25
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-10-02 10:17:54 -0700

    Initial project version

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+  
+      <Text style={{fontWeight:900, fontSize:50}}>Gym Bro</Text>
+      {<StatusBar style="auto" />}
+  
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
