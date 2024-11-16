c4054d68bcbc9815bf369750ed041aa587c269ec
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-11-16 05:13:09 +0000

    .

--- src/screens/LoginScreen/styles/button.js
+++ src/screens/LoginScreen/styles/button.js
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     authButton: {
-        flex: 1,
         backgroundColor: 'blue',
         alignContent: 'center',
         justifyContent: 'center',
         fontWeight: 'bold',
-        borderRadius: 100
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth: 2
       },
 });
--- src/screens/LoginScreen/styles/text.js
+++ src/screens/LoginScreen/styles/text.js
@@ -1,10 +1,11 @@
 import { StyleSheet } from 'react-native';
+import colors from "../../../constants/colors.json"
 
 export default StyleSheet.create({
     title: {
         fontWeight: '800',
         letterSpacing: 1,
-        fontSize: 60,
+        fontSize: 70,
         color: 'white'
     },
 });
