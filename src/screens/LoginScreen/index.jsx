719973d00ba638426280cbb6da2502e7c696a46f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-12-15 09:50:54 -0800

    Refactored login page

--- src/screens/LoginScreen/index.jsx
+++ src/screens/LoginScreen/index.jsx
@@ -1,42 +1,47 @@
-import { View, SafeAreaView, Text, StyleSheet } from "react-native";
-import CustomInput from "./components/CustomInput";
-import AuthButton from "./components/AuthButton";
-import styles from './styles'
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import styles from "./styles";
+import CustomInput from './components/CustomInput';
+import Switch from './components/Switch';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import AuthButton from './components/AuthButton';
+import handleLogin from '../../helper/handleLogin';
 
-export default LoginScreen = () => {
-    return (
-        <>
-            <SafeAreaView style={styles.container.safeContainer}>
-              <Text style={[styles.text.title, {borderBottomColor:'red', borderBottomWidth: 2}]}>Login</Text>
-              <StatusBar style='auto'/>
-            </SafeAreaView>
-            
-            <View style={styles.container.inputContainer}>
-            
-                <CustomInput 
-                    title={'Username'}
-                    secure={false}
-                    placeholder={'ex: JohnDoe'}
-                />
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-                <View style={{borderWidth: StyleSheet.hairlineWidth,
-                        backgroundColor: 'black',
-                        height: 0.0001,
-                        width: '90%'
-                }}/>
-                
-                <CustomInput 
-                    title={'Password'}
-                    secure={true}
-                    placeholder={'ex: 12345'}
-                    
-                />
-            </View>
+  return (
+    <SafeAreaView style={styles.root}>
 
-            <SafeAreaView style={[styles.container.safeContainer, {backgroundColor:'orange'}]}>
-                <AuthButton />
-            </SafeAreaView>
-        </>
-    );    
-};
+          <Text style={styles.title}>GymBro</Text>
+
+          <Switch
+            states={["Login", "Sign Up"]}
+          />
+
+          <View style={{height:20}}/>
+
+          <CustomInput 
+          placeholder="Username"
+          onChange={setUsername}
+          />
+
+          <CustomInput 
+          secure = {true}
+          placeholder="Password"
+          onChange={setPassword}
+          />
+
+          <AuthButton 
+          text={"Login"} 
+          onPress={
+            ()=>{
+              const {success} = handleLogin(username, password)
+              if (success) {Alert.alert("Success")}
+          }} />
+
+
+    </SafeAreaView>
+  );
+}
