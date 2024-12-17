d881d0e572b14a0bd6cea68bd4983425c6b60c0f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-12-17 08:42:16 -0800

    Wrapped in Authentication Context Provider

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { AuthProvider } from "./src/contexts/AuthContext";
 
 export default App = () => {
   return (
+    <AuthProvider>
     <SafeAreaProvider>
       <AppNavigator />
     </SafeAreaProvider>
+    </AuthProvider>
   );
 }
