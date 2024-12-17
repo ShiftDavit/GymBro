331e82725c8136b269e8f3544eeb05a30a634f5d
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-12-17 08:43:56 -0800

    Added conditional rendering based on Auth Context

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,8 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./AuthStack";
+import { AuthContext, AuthProvider} from "../contexts/AuthContext";
+import { useContext, useEffect } from "react";
+import DashboardScreen from "../screens/DashboardScreen";
 
-export default AppNavigator = () => (
-    <NavigationContainer>
-        <AuthStack />
-    </NavigationContainer>
-);
+export default AppNavigator = () => {
+    const {isLoggedIn} = useContext(AuthContext);
+
+    return (
+        <NavigationContainer>
+            {isLoggedIn?<DashboardScreen />:<AuthStack />}
+        </NavigationContainer>
+    )
+};
