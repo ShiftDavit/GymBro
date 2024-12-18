d06276977e6d4fbb9748e111e47b90b8a0430707
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-12-17 23:10:56 -0800

    Placeholder screen for settings

---
+++ src/screens/SettingsScreen/index.jsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default SettingsScreen = () => {
+    return(
+        <SafeAreaView><Text>Settings</Text></SafeAreaView>
+    );
+};
