8c27789b429763119c48b81c73e04ae972499739
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-12-17 23:11:19 -0800

    Added Safe Area support

--- src/screens/DashboardScreen/index.jsx
+++ src/screens/DashboardScreen/index.jsx
@@ -1,7 +1,8 @@
 import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default DashboardScreen = () => {
     return(
-        <Text>Dashboard</Text>
+        <SafeAreaView><Text>Dashboard</Text></SafeAreaView>
     );
 };
