7daa1dd59c906feadcfb6bf894646e309d430ee8
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2025-05-06 17:17:13 +0000

    Auto adjust the status bar color to match the theme

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import { StatusBar } from 'expo-status-bar';
 
 export default App = () => {
   return (
     <AuthProvider>
     <SafeAreaProvider>
       <AppNavigator />
+      <StatusBar style="auto" />
     </SafeAreaProvider>
     </AuthProvider>
   );
