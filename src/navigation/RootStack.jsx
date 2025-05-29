273eaeb17aa3aef396e5fafcbe1ee57456117a85
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2025-05-29 09:37:16 +0000

    Wrapped in ExerciseListContextProvider

--- src/navigation/RootStack.jsx
+++ src/navigation/RootStack.jsx
@@ -1,10 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import MainTabs from "./MainTabs";
 import LogExerciseScreen from "../screens/LogExerciseScreen"
+import { ExerciseListContextProvider } from '../contexts/ExerciseListContext';
 
 const Stack = createStackNavigator();
 
 export default AuthStack = () => (
+    <ExerciseListContextProvider>
     <Stack.Navigator>    
         <Stack.Screen 
             name="MainTabs"
@@ -14,4 +16,6 @@ export default AuthStack = () => (
               }}/>
         <Stack.Screen name="LogExerciseScreen" component={LogExerciseScreen}/>
     </Stack.Navigator>
+    </ExerciseListContextProvider>
+
 );
